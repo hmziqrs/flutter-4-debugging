e960e8afb73f4dc02800288564295062e3511be1
hackerhgl <hackerhgl@gmail.com>
2020-08-12 18:20:00 +0500

    Github Actions patch

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -19,7 +19,7 @@ static void my_application_activate(GApplication* application) {
   gtk_header_bar_set_title(header_bar, "wowo");
   gtk_header_bar_set_show_close_button(header_bar, TRUE);
   gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
-  gtk_window_set_default_size(window, 1280, 720);
+  gtk_window_maximize(window);
   gtk_widget_show(GTK_WIDGET(window));
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();
@@ -40,7 +40,5 @@ static void my_application_class_init(MyApplicationClass* klass) {
 static void my_application_init(MyApplication* self) {}
 
 MyApplication* my_application_new() {
-  return MY_APPLICATION(g_object_new(my_application_get_type(),
-                                     "application-id", APPLICATION_ID,
-                                     nullptr));
+  return MY_APPLICATION(g_object_new(my_application_get_type(), nullptr));
 }
